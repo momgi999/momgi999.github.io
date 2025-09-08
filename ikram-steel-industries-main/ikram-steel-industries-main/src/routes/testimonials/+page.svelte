b134fdc9ef6b5bca268ee0b54862c31470c58a7e
momgi999 <youpcs999@gmail.com>
2025-09-08 02:08:36 +0200

    Add files via upload

---
+++ ikram-steel-industries-main/ikram-steel-industries-main/src/routes/testimonials/+page.svelte
@@ -0,0 +1,18 @@
+<script>
+	import BillBoard from '$components/BillBoard.svelte';
+	import Testimonials from '$components/Testimonials.svelte';
+	import billboard from '$assets/gallery/gallery05.webp?enhanced&w=1600;800;400&format=avif;webp';
+</script>
+
+<svelte:head>
+	<title>Testimonials - Ikram Steel Industries</title>
+	<meta
+		name="description"
+		content="Ikram Steel Industries aims to be a global leader in the steel manufacturing sector, driven by innovation, quality, and customer satisfaction. We are dedicated to building a sustainable future through our products and practices, contributing to the growth and development of the construction industry."
+	/>
+</svelte:head>
+
+<div class="row">
+	<BillBoard billBoardPic={billboard} />
+	<Testimonials />
+</div>
---
+++ ikram-steel-industries-main/ikram-steel-industries-main/vite.config.js
@@ -0,0 +1,64 @@
+// const IN_PRODUCTION = process.env.NODE_ENV === 'production';
+import { defineConfig } from 'vite';
+import { sveltekit } from '@sveltejs/kit/vite';
+import { enhancedImages } from '@sveltejs/enhanced-img';
+import { purgeCSSPlugin } from '@fullhuman/postcss-purgecss';
+import cssnano from 'cssnano';
+import autoprefixer from 'autoprefixer';
+import path from 'path';
+const IN_PRODUCTION = process.env.NODE_ENV === 'production';
+// const bootstrap = 'node_modules/bootstrap';
+
+export default defineConfig({
+	plugins: [enhancedImages(), sveltekit()],
+
+	resolve: {
+		alias: {
+			$lib: path.resolve('./src/lib'),
+			$components: path.resolve('./src/components'),
+			$styles: path.resolve('./src/styles'),
+			$assets: path.resolve('./src/assets')
+		}
+	},
+
+	css: {
+		preprocessorOptions: {
+			scss: {
+				api: 'modern'
+			}
+		},
+		postcss: {
+			plugins: IN_PRODUCTION
+				? [
+						purgeCSSPlugin({
+							content: ['./src/app.html', './src/**/*.js', './src/**/*.svelte'],
+							safelist: [/svelte-/],
+							defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
+							keyframes: true,
+							variables: true
+						}),
+						cssnano({
+							preset: ['default', { discardComments: { removeAll: true } }]
+						}),
+						autoprefixer()
+					]
+				: []
+		}
+	},
+
+	// base: './',
+	server: {
+		port: 3000
+	},
+
+	build: {
+		// outDir: './build',
+		emptyOutDir: true,
+		minify: 'terser',
+		terserOptions: {
+			format: {
+				comments: false
+			}
+		}
+	}
+});
